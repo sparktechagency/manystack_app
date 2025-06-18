53763afceeaf0af266daa5c8265aeb71b25eac9c
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-18 14:46:07 +0600

    intervention api call hooks create

---
+++ src/hooks/interventionApiCall.tsx
@@ -0,0 +1,30 @@
+import Toast from 'react-native-toast-message';
+import { useCreateInterventionMutation } from '../redux/Apis/interventionApis';
+
+export const createIntervention = () => {
+  const [create, { isLoading }] = useCreateInterventionMutation();
+  const handleCreateIntervention = async (data: any) => {
+    try {
+      await create(data).unwrap()
+        .then((res) => {
+          Toast.show({
+            type: 'success',
+            text1: 'intervention created',
+            text2: res.data?.message || 'Intervention created successfully.',
+          })
+        }).catch((err) => {
+          Toast.show({
+            type: 'error',
+            text1: 'Failed to create intervention',
+            text2: err.data?.message || 'Failed to create intervention.',
+          })
+        });
+      return true;
+    } catch (err) {
+      // throw err;
+      return false;
+    }
+  }
+
+  return { handleCreateIntervention, isLoading };
+}
