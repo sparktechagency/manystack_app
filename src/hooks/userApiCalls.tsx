bed801d49915edc5de09685d5ac13da858b52928
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-26 12:26:41 +0600

    change password api call created

---
+++ src/hooks/userApiCalls.tsx
@@ -0,0 +1,43 @@
+import Toast from 'react-native-toast-message';
+import { useChangePasswordMutation, useUpdateProfileMutation } from '../redux/Apis/userApis';
+
+export const useUpdateProfile = () => {
+  const [updateProfile, { isLoading }] = useUpdateProfileMutation();
+  const updateProfileHandler = (data: any, id: string, handler?: () => void) => {
+    updateProfile({ data, id }).then((res) => {
+      Toast.show({
+        type: 'success',
+        text1: 'Profile updated',
+        text2: res.data?.message || 'Profile updated successfully.',
+      })
+      handler?.()
+    }).catch((err) => {
+      Toast.show({
+        type: 'error',
+        text1: 'Failed to update profile',
+        text2: err.data?.message || 'Failed to update profile.',
+      })
+    })
+  }
+  return { updateProfileHandler, isLoading }
+}
+export const useChangePassword = () => {
+  const [changePassword, { isLoading }] = useChangePasswordMutation();
+  const changePasswordHandler = (data: any, handler?: () => void) => {
+    changePassword(data).then((res: any) => {
+      Toast.show({
+        type: 'success',
+        text1: 'Password changed',
+        text2: res?.message || 'Password changed successfully.',
+      })
+      handler?.()
+    }).catch((err) => {
+      Toast.show({
+        type: 'error',
+        text1: 'Failed to change password',
+        text2: err?.message || 'Failed to change password.',
+      })
+    })
+  }
+  return { changePasswordHandler, isLoading }
+}
