07b60ade50d14b1068bb3fff0848d0555f460aed
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-07-15 12:35:23 +0600

    url

--- src/utils/baseUrls.ts
+++ src/utils/baseUrls.ts
@@ -1,5 +1,6 @@
-export const baseUrl = 'http://10.0.60.70:5000';
-// export const baseUrl = 'http://10.0.60.19:5000';
+// export const baseUrl = 'http://10.0.60.70:5000';
+export const baseUrl = 'http://13.39.251.121';
+
 export const generateImageUrl = (imagePath: string): string =>
   imagePath?.includes('http')
     ? imagePath
