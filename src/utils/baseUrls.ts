27392d03a0db2e070633e5d28ae22b1ed3c7a5da
Your Namecd <shaharulsiyam0273@gmail.com>
2025-07-20 15:56:02 +0600

    s

--- src/utils/baseUrls.ts
+++ src/utils/baseUrls.ts
@@ -1,9 +1,11 @@
 // export const baseUrl = 'http://10.0.60.70:5000';
 export const baseUrl = 'http://13.39.251.121';
 
+
 export const generateImageUrl = (imagePath: string): string =>
   imagePath?.includes('http')
     ? imagePath
     : imagePath?.startsWith('/')
       ? `${baseUrl}${imagePath}`
       : `${baseUrl}/${imagePath}`;
+
