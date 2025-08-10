d73ab6ab326b1c70b567024587079ce831002454
Your Namecd <shaharulsiyam0273@gmail.com>
2025-08-10 12:38:37 +0600

    delete api complte

--- src/hooks/authApisCall.tsx
+++ src/hooks/authApisCall.tsx
@@ -1,8 +1,9 @@
 import Toast from 'react-native-toast-message';
-import {useForgetPasswordMutation} from '../redux/Apis/authApis';
+import { useForgetPasswordMutation } from '../redux/Apis/authApis';
+import { useDeleteAccountMutation } from '../redux/Apis/userApis';
 
 export const useForgetPassword = () => {
-  const [forgetPassword, {isLoading}] = useForgetPasswordMutation();
+  const [forgetPassword, { isLoading }] = useForgetPasswordMutation();
   const forgetPasswordHandler = (data: any, handler?: () => void) => {
     forgetPassword(data)
       .unwrap()
@@ -23,5 +24,28 @@ export const useForgetPassword = () => {
         });
       });
   };
-  return {forgetPasswordHandler, isLoading};
+  return { forgetPasswordHandler, isLoading };
 };
+
+export const useDeleteAccount = () => {
+  const [deleteAcc, { isLoading }] = useDeleteAccountMutation()
+  const deleteAccount = (handler?: () => void) => {
+    deleteAcc(undefined).then((res: any) => {
+      Toast.show({
+        type: 'success',
+        text1: 'account delete successfully',
+        text2: res?.message || 'account delete successfully',
+      });
+
+      handler?.();
+    })
+      .catch(err => {
+        Toast.show({
+          type: 'error',
+          text1: 'Failed to delete account',
+          text2: err?.data?.message || 'Failed to delete account',
+        });
+      });
+  }
+  return { deleteAccount, isLoading }
+}
