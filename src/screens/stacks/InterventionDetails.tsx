ce92a43e259a625f63e1ee728d74e0854ca12582
shaharulsiyam <shaharulsiyam0273@gmail.com>
2025-09-17 12:30:08 +0600

    s

--- src/screens/stacks/InterventionDetails.tsx
+++ src/screens/stacks/InterventionDetails.tsx
@@ -2,7 +2,7 @@ import {
   useRoute
 } from '@react-navigation/native';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import FlexTextOpacity from '../../components/InterventionDetails/FlexTextOpacity';
 import ImageCard from '../../components/InterventionDetails/ImageCard';
 import BackButton from '../../components/sheard/BackButton';
@@ -15,7 +15,7 @@ import { t } from '../../utils/translate';
 const InterventionDetails = () => {
   const { width, height, themeColors, currency, english } = useGlobalContext();
   const { params }: any = useRoute();
-  const { data } = useGetInterventionByIdQuery(params?.params?.id);
+  const { data, isLoading, isFetching } = useGetInterventionByIdQuery(params?.params?.id);
   return (
     <View
       style={{
@@ -24,76 +24,87 @@ const InterventionDetails = () => {
         paddingHorizontal: 16,
       }}>
       <BackButton text={t('details', english)} />
-      <Text
-        style={{
-          fontSize: 16,
-          fontWeight: 600,
-          color: themeColors.primary as string,
-        }}>
-        {data?.intervention?.interventionId}
-      </Text>
-
-      <FlexTextOpacity
-        text1="Date :"
-        text2={data?.intervention?.createdAt?.split('T')[0]}
-      />
-      <FlexTextOpacity
-        text1="Category :"
-        text2={data?.intervention?.category?.name}
-      />
-      <FlexTextOpacity
-        text1="Price :"
-        text2={`${currency}${data?.intervention?.price}`}
-        color={themeColors.primary as string}
-      />
-      <FlexTextOpacity text1="Note :" text2={data?.intervention?.note} />
-      <FlatList
-        data={data?.intervention?.images ?? []}
-        numColumns={2}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <ImageCard key={item} item={item} id={data?.intervention?._id} />
-        )}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          gap: 15,
-          paddingVertical: 0,
-          paddingHorizontal: 0,
-          paddingBottom: 170,
-          marginTop: 20,
-        }}
-      />
-      <View
-        style={{
-          paddingHorizontal: 25,
-          position: 'absolute',
-          bottom: 100,
-          width: width,
-          paddingVertical: 16,
-          // backgroundColor: hexToRGBA(themeColors.white as string, 0.9),
-        }}>
-        <GradientButton
-          handler={() => {
-            downloadButton(
-              `api/intervention/download-pdf/${data?.intervention?._id}`,
-              'Intervention',
-            );
+      {
+        (isLoading || isFetching) ? <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
           }}>
+          <ActivityIndicator size="large" color="blue" />
+        </View> : <>
           <Text
             style={{
-              color: 'white',
-              textAlign: 'center',
-              fontWeight: 700,
-              fontSize: 18,
+              marginTop: 20,
+              fontSize: 16,
+              fontWeight: 600,
+              color: themeColors.primary as string,
             }}>
-            Export PDF
+            {data?.intervention?.interventionId}
           </Text>
-        </GradientButton>
-      </View>
+
+          <FlexTextOpacity
+            text1="Date :"
+            text2={data?.intervention?.createdAt?.split('T')[0]}
+          />
+          <FlexTextOpacity
+            text1="Category :"
+            text2={data?.intervention?.category?.name}
+          />
+          <FlexTextOpacity
+            text1="Price :"
+            text2={`${currency}${data?.intervention?.price}`}
+            color={themeColors.primary as string}
+          />
+          <FlexTextOpacity text1="Note :" text2={data?.intervention?.note} />
+          <FlatList
+            data={data?.intervention?.images ?? []}
+            numColumns={2}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <ImageCard key={item} item={item} id={data?.intervention?._id} />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              gap: 15,
+              paddingVertical: 0,
+              paddingHorizontal: 0,
+              paddingBottom: 170,
+              marginTop: 20,
+            }}
+          />
+          <View
+            style={{
+              paddingHorizontal: 25,
+              position: 'absolute',
+              bottom: 100,
+              width: width,
+              paddingVertical: 16,
+              // backgroundColor: hexToRGBA(themeColors.white as string, 0.9),
+            }}>
+            <GradientButton
+              handler={() => {
+                downloadButton(
+                  `api/intervention/download-pdf/${data?.intervention?._id}`,
+                  'Intervention',
+                );
+              }}>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  fontWeight: 700,
+                  fontSize: 18,
+                }}>
+                Export PDF
+              </Text>
+            </GradientButton>
+          </View>
+        </>
+      }
+
     </View>
   );
 };
 
 export default InterventionDetails;
-
-const styles = StyleSheet.create({});
--- src/utils/DownloadPdf.ts
+++ src/utils/DownloadPdf.ts
@@ -1,16 +1,16 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ReactNativeBlobUtil from 'react-native-blob-util';
 import Toast from 'react-native-toast-message';
-import RNFetchBlob from 'rn-fetch-blob';
 import { baseUrl } from './baseUrls';
 
 export const downloadButton = async (url: string, name?: string) => {
   console.log('download pdf button clicked');
   const token = await AsyncStorage.getItem('token');
   try {
-    let dirs = RNFetchBlob.fs.dirs;
+    let dirs = ReactNativeBlobUtil.fs.dirs;
     const fileName = `${name ?? 'expanses'}-${Date.now()}.pdf`;
 
-    RNFetchBlob.config({
+    ReactNativeBlobUtil.config({
       fileCache: true,
       addAndroidDownloads: {
         useDownloadManager: true,
@@ -20,6 +20,7 @@ export const downloadButton = async (url: string, name?: string) => {
         mime: 'application/pdf',
         path: dirs.DownloadDir + '/' + fileName,
         mediaScannable: true,
+
       },
     })
       .fetch('GET', `${baseUrl}/${url}`, {
